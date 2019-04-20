c846f8d3534a6b1f87741b94a6950f674b329430
Scorbutics <brice.bulgarelli@outlook.com>
2019-04-20 14:36:00 +0200

    chore : adds a sprite & bitmap to resize_screen unit-test

--- unit-test/test_graphics.rb
+++ unit-test/test_graphics.rb
@@ -1,8 +1,8 @@
-#encoding: ASCII-8BIT
 # Load required library
 require_relative "../lib/LiteRGSS_test"
 include LiteRGSS
 require "rspec"
+require "base64"
 
 # Start specification
 RSpec.describe Graphics do
@@ -20,8 +20,14 @@ RSpec.describe Graphics do
 
   it 'resize_screen' do
     Graphics.start
+    @a = Sprite.new()
+    rawBmp = Base64.decode64("iVBORw0KGgoAAAANSUhEUgAAAAUA
+    AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
+        9TXL0Y4OHwAAAABJRU5ErkJggg==")
+    @a.bitmap = Bitmap.new(rawBmp, true)
+
     Graphics.update
-    Graphics.resize_screen(1280, 720)
+    Graphics.resize_screen(1280, 720)    
     Graphics.update
   end
   
